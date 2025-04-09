11f780e222e546619cee73a2f71bd5a7a3360cfc
Naji-Ullah <161580987+Naji-Ullah@users.noreply.github.com>
2025-04-10 00:15:36 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar"  ;
+
+function App() {
+  console.log("App component rendered");
+  return (
+    <>
+      <Navbar />
+      
+    </>
+  );
+}
+
+export default App;
